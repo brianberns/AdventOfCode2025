1a21a0719b83369aea5c68160f23cb364a04e78f
Brian Berns <brianberns@gmail.com>
2025-12-01 09:14:21 -0500

    Improved assertion.

--- Program.fs
+++ Program.fs
@@ -2,10 +2,14 @@
 
 module Program =
 
+    let check expected actual =
+        if actual <> expected then
+            printfn $"*** Failure: Expected: {expected}, but received {actual}"
+
     let day1_part1 = Day1.part1 "Day1.input.txt"
-    assert(day1_part1 = 1084)
     printfn "Day 1, part 1: %A" day1_part1
+    check 1084 day1_part1
 
     let day1_part2 = Day1.part2 "Day1.input.txt"
-    assert(day1_part2 = 6475)
     printfn "Day 1, part 2: %A" day1_part2
+    check 6475 day1_part2
