03cc6138115ab67fc6ca2e93336a167101340a7d
Brian Berns <brianberns@gmail.com>
2025-12-02 01:33:19 -0500

    Optimize for speed.

--- Day2.fs
+++ Day2.fs
@@ -14,17 +14,24 @@ module Day2 =
                 Int64.Parse split[0],
                 Int64.Parse split[1])
 
+    let getInputs path =
+        parseFile path
+            |> Array.collect (fun (a, b) ->
+                assert(b >= a)
+                [| a .. b |])
+
     let isValid nChunks (str : string) =
         if nChunks > 1 && str.Length % nChunks = 0 then
             let size = str.Length / nChunks
-            str
-                |> Seq.chunkBySize size
-                |> Seq.distinct
-                |> Seq.length > 1
+            str.ToCharArray()
+                |> Array.chunkBySize size
+                |> Array.map String
+                |> Array.distinct
+                |> Array.length > 1
         else true
 
     let getDivisors n =
-        Array.sort [|
+        [|
             for m = 1 to int (sqrt (float n)) do
                 if n % m = 0 then
                     yield m
@@ -34,21 +41,15 @@ module Day2 =
 
     let isValidAll (str : string) =
         getDivisors str.Length
-            |> Seq.forall (fun n ->
+            |> Array.forall (fun n ->
                 isValid n str)
 
     let part1 path =
-        parseFile path
-            |> Seq.collect (fun (a, b) ->
-                assert(b >= a)
-                seq { a .. b })
-            |> Seq.where (string >> isValid 2 >> not)
-            |> Seq.sum
+        getInputs path
+            |> Array.where (string >> isValid 2 >> not)
+            |> Array.sum
 
     let part2 path =
-        parseFile path
-            |> Seq.collect (fun (a, b) ->
-                assert(b >= a)
-                seq { a .. b })
-            |> Seq.where (string >> isValidAll >> not)
-            |> Seq.sum
+        getInputs path
+            |> Array.where (string >> isValidAll >> not)
+            |> Array.sum
