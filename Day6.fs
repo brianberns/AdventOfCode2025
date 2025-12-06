6ddb9815dce14a69833a5e9cb41017007493ca1a
Brian Berns <brianberns@gmail.com>
2025-12-06 02:04:16 -0500

    Shrank.

--- Day6.fs
+++ Day6.fs
@@ -9,24 +9,20 @@ module Day6 =
         line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
 
     let parseOps (lines : string[]) : (int64 -> int64 -> int64)[] =
-        Array.last lines
-            |> split
+        split (Array.last lines)
             |> Array.map (function "+" -> (+) | "*" -> (*))
 
-    let parseFile1 path =
+    let part1 path =
         let lines = File.ReadAllLines(path)
         let inputRows =
             lines[.. lines.Length - 2]
                 |> Array.map (split >> Array.map Int64.Parse)
-        inputRows, parseOps lines
-
-    let part1 path =
-        let inputRows, ops = parseFile1 path
+        let ops = parseOps lines
         (ops, Array.transpose inputRows)
             ||> Array.map2 Array.reduce
             |> Array.sum
 
-    let parseFile2 path =
+    let part2 path =
         let lines = File.ReadAllLines(path)
         let strs =
             lines[0 .. lines.Length - 2]
@@ -38,10 +34,6 @@ module Day6 =
                 ||> Array.foldBack (fun str (head :: tail) ->
                     if str = "" then [] :: (head :: tail)     // start a new chunk
                     else (Int64.Parse str :: head) :: tail)   // append to current chunk
-        inputs, List.ofArray (parseOps lines)
-
-    let part2 path =
-        let inputs, ops = parseFile2 path
-        (ops, inputs)
+        (List.ofArray (parseOps lines), inputs)
             ||> List.map2 List.reduce
             |> List.sum
