ef75aa6c63f269391110f5219d4331f9ca9d144c
Brian Berns <brianberns@gmail.com>
2025-12-07 02:01:36 -0500

    Shrink.

--- Day7.fs
+++ Day7.fs
@@ -8,23 +8,18 @@ module Day7 =
 
     let update (world : _[][]) iter =
         let splitterCols =
-            set [
-                for col, (item : Item) in world[iter + 1] do
-                    if item.IsSplitter then col : int
-                    else ()
-            ]
+            set [ for col, (item : Item) in world[iter + 1] do
+                    if item.IsSplitter then col : int else () ]
         let nextRow =
-            [|
-                for col, item in world[iter] do
-                    match item with
-                        | Beam n ->
-                            if splitterCols.Contains(col) then
-                                yield col - 1, Beam n; yield col + 1, Beam n
-                                yield col, Splitter
-                            else
-                                yield col, Beam n
-                        | _ -> ()
-            |]
+            [| for col, item in world[iter] do
+                match item with
+                    | Beam n ->
+                        if splitterCols.Contains(col) then
+                            yield col - 1, Beam n; yield col + 1, Beam n
+                            yield col, Splitter
+                        else
+                            yield col, Beam n
+                    | _ -> () |]
                 |> Array.groupBy fst
                 |> Array.map (fun (col, group) ->
                     let items = Array.map snd group
@@ -36,15 +31,12 @@ module Day7 =
 
     let parseFile path =
         let lines = File.ReadAllLines(path)
-        [|
-            for line in lines do [|
-                for col, c in Array.indexed (line.ToCharArray()) do
-                    match c with
-                        | 'S' -> col, Beam 1
-                        | '^' -> col, Splitter
-                        | '.' -> ()
-            |]
-        |]
+        [| for line in lines do
+            [| for col, c in Array.indexed (line.ToCharArray()) do
+                match c with
+                    | 'S' -> col, Beam 1
+                    | '^' -> col, Splitter
+                    | '.' -> () |] |]
 
     let run (world : _[][]) =
         Array.fold update world [| 0 .. world.Length - 2 |]
@@ -52,10 +44,9 @@ module Day7 =
     let part1 path =
         parseFile path
             |> run
-            |> Array.sumBy (fun posItems ->
-                posItems
-                    |> Array.where (snd >> _.IsSplitter)
-                    |> Array.length)
+            |> Array.sumBy (
+                Array.where (snd >> _.IsSplitter)
+                    >> Array.length)
 
     let part2 path =
         parseFile path
