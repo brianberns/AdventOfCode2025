cce3b1547f9245c3951a94491854969674e79be0
Brian Berns <brianberns@gmail.com>
2025-12-08 01:24:38 -0500

    Shrink.

--- Day8.fs
+++ Day8.fs
@@ -11,40 +11,39 @@ module Day8 =
             |> sqrt
 
     let parseFile path =
-        let pointMap =
+        let locs =
             File.ReadAllLines(path)
                 |> Seq.mapi (fun id line ->
                     let chunks =
                         line.Split(',') |> Array.map Int64.Parse
                     id, (chunks[0], chunks[1], chunks[2]))
                 |> Map
-        let graph =
-            pointMap.Keys
+        let adj =
+            locs.Keys
                 |> Seq.map (fun id -> id, Set.empty)
                 |> Map
-        let pointDists =
+        let dists =
             List.sortBy snd [
-                for i = 0 to pointMap.Count - 2 do
-                    for j = i + 1 to pointMap.Count - 1 do
-                        (i, j), dist pointMap[i] pointMap[j]
-            ]
-        pointMap, pointDists, graph
+                for i = 0 to locs.Count - 2 do
+                    for j = i + 1 to locs.Count - 1 do
+                        (i, j), dist locs[i] locs[j] ]
+        locs, dists, adj
 
-    let connect idA idB (graph : Map<_, Set<_>>) =
-        graph
-            |> Map.add idA (graph[idA].Add(idB))
-            |> Map.add idB (graph[idB].Add(idA))
+    let connect idA idB (adj : Map<_, Set<_>>) =
+        adj
+            |> Map.add idA (adj[idA].Add(idB))
+            |> Map.add idB (adj[idB].Add(idA))
 
-    let update (((idA, idB), _) :: rest) graph =
-        rest, connect idA idB graph
+    let update (((idA, idB), _) :: rest) adj =
+        rest, connect idA idB adj
 
-    let getCircuits (graph : Map<_, _>) =
+    let getCircuits (adj : Map<_, _>) =
 
         let rec walk (seen : Set<_>) id =
             if seen.Contains(id) then seen
-            else Seq.fold walk (seen.Add(id)) graph[id]
+            else Seq.fold walk (seen.Add(id)) adj[id]
 
-        set graph.Keys
+        set adj.Keys
             |> Seq.unfold (fun unseen ->
                 if unseen.IsEmpty then None
                 else
@@ -52,25 +51,24 @@ module Day8 =
                     Some (seen, unseen - seen))
 
     let part1 path =
-        let _, pointDists, graph = parseFile path
-        let _, graph =
-            ((pointDists, graph), [1..1000])
-                ||> Seq.fold (fun (pointDists, graph) _ ->
-                    update pointDists graph)
-        getCircuits graph
+        let _, dists, adj = parseFile path
+        let _, adj =
+            ((dists, adj), [1..1000])
+                ||> Seq.fold (fun (dists, adj) _ -> update dists adj)
+        getCircuits adj
             |> Seq.map _.Count
             |> Seq.sortDescending
             |> Seq.take 3
             |> Seq.reduce (*)
 
-    let rec updateLoop ((((idA, idB), _) :: _) as pointDists) graph =
-        let pointDists, graph = update pointDists graph
-        if getCircuits graph |> Seq.length = 1 then idA, idB
-        else updateLoop pointDists graph
+    let rec updateLoop ((((idA, idB), _) :: _) as dists) adj =
+        let dists, adj = update dists adj
+        if getCircuits adj |> Seq.length = 1 then idA, idB
+        else updateLoop dists adj
 
     let part2 path =
-        let pointMap, pointDists, graph = parseFile path
-        let idA, idB = updateLoop pointDists graph
-        let xa, _, _ = pointMap[idA]
-        let xb, _, _ = pointMap[idB]
+        let locs, dists, adj = parseFile path
+        let idA, idB = updateLoop dists adj
+        let xa, _, _ = locs[idA]
+        let xb, _, _ = locs[idB]
         xa * xb
